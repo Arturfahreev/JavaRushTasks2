5b5b3d180e64e75de5da32933cc671a9d8748cb4
arturfahreev <jd-forever@mail.ru>
2023-03-24 13:55:04 +0300

    task1916

--- JavaRush2/JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ JavaRush2/JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -26,61 +26,48 @@ public class Solution {
             while (reader2.ready()) {
                 list2.add(reader2.readLine());
             }
-            for (int i = 0; i < Math.min(list1.size(), list2.size()); i++ ) {
-                String line1List1 = list1.get(i);
-                String line2List1 = list1.get(i + 1);
-                //String line3List1 = list1.get(i + 2);
 
-                String line1List2 = list2.get(i);
-                String line2List2 = list2.get(i + 1);
-                //String line3List2 = list2.get(i + 2);
-//                строка1         строка1            SAME строка1
-//                строка2         строка3            REMOVED строка2
-//                строка3         строка5            SAME строка3
-//                строка4         строка0           REMOVED строка4
-//                строка5         строка1           SAME строка5
-//                строка1          строка3            ADDED строка0
-//                 строка2         строка4                SAME строка1
-//                 строка3         строка5              REMOVED строка2
-//                 строка5                              SAME строка3
-//                 строка0                             ADDED строка4
-//                                                   SAME строка5
-//                                                    REMOVED строка0
+            String line1List1 = "";
+            String line2List1 = "";
+            String line1List2 = "";
+            String line2List2 = "";
+
+            while(list1.size() > 0 && list2.size() > 0) {
+                line1List1 = list1.get(0);
+                if (list1.size() > 1) {
+                    line2List1 = list1.get(1);
+                }
+
+                line1List2 = list2.get(0);
+                if (list2.size() > 1) {
+                    line2List2 = list2.get(1);
+                }
 
                 if (line1List1.equals(line1List2)) {
                     lines.add(new LineItem(Type.SAME, line1List1));
-
+                    list1.remove(0);
+                    list2.remove(0);
 
                 } else if (line2List1.equals(line1List2)) {
                     lines.add(new LineItem(Type.REMOVED, line1List1));
-                    lines.add(new LineItem(Type.SAME, line2List1));
+                    list1.remove(line1List1);
+
                 } else if (line1List1.equals(line2List2)) {
                     lines.add(new LineItem(Type.ADDED, line1List2));
-                    lines.add(new LineItem(Type.SAME, line1List1));
+                    list2.remove(line1List2);
                 }
             }
-
-
-            String line1File1;
-            String line1File2;
-            String line2File1;
-            String line2File2;
-            while (reader1.ready() || reader2.ready()) {
-                if (reader1.ready() && reader2.ready()) {
-                    line1File1 = reader1.readLine();
-                    line1File2 = reader2.readLine();
-                    if (line1File1.equals(line1File2)) {
-                        lines.add(new LineItem(Type.SAME, line1File1));
-                    } else {
-                        line2File1 = reader1.readLine();
-                        line2File2 = reader2.readLine();
-                        if (line2File1.equals(line1File2)) {
-                            lines.add(new LineItem(Type.REMOVED, line1File1));
-                            lines.add(new LineItem(Type.SAME, line2File1));
-                        }
-                    }
-                }
+            if (list1.isEmpty() && !list2.isEmpty()) {
+                lines.add(new LineItem(Type.ADDED, list2.get(0)));
             }
+            if (!list1.isEmpty() && list2.isEmpty()) {
+                lines.add(new LineItem(Type.REMOVED, list1.get(0)));
+            }
+            for (LineItem lineItem : lines) {
+                System.out.println(lineItem.getLine());
+
+            }
+
         }
     }
 
@@ -99,5 +86,22 @@ public class Solution {
             this.type = type;
             this.line = line;
         }
+        public String getLine () {
+            return this.type + " " + this.line;
+        }
     }
 }
+
+//                строка1         строка1            SAME строка1
+//                строка2         строка3            REMOVED строка2
+//                строка3         строка5            SAME строка3
+//                строка4         строка0            REMOVED строка4
+//                строка5         строка1            SAME строка5
+//                строка1         строка3            ADDED строка0
+//                строка2         строка4            SAME строка1
+//                строка3         строка5            REMOVED строка2
+//                строка5                            SAME строка3
+//                строка0                            ADDED строка4
+//                                                   SAME строка5
+//                                                   REMOVED строка0
+
